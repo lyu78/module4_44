ac3c80c710f1abbc521b0d138285684b571aa641
Иван Иванович <ivan@ivan.ru>
2023-07-22 13:45:13 +0300

    Код с лекции №7

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+
+    # Товар
+    # строковое поле для небольших размеров
+    # 'заголовок' - verbose_name - название поля извне
+    title = models.CharField('заголовок', max_length=128)
+
+    # Описание товара/информация о товаре
+    # Большое текстовое поле, для больших текстов
+    description = models.TextField('описание')
+
+    # Цена
+    # Специальный тип данных с фиксированной точкой
+    price = models.DecimalField('цена', max_digits=10, decimal_places=2)
+
+    # Уместен ли торг
+    # Логический тип, два значения - правда или ложь
+    auction = models.BooleanField('торг', help_text='Отметьте, уместен ли торг')
+
+    # Дата публикации
+    # Поле записывается при создании объявления
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Дата изменения/обновления
+    # Поле записывается при каждом обновлении
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Имя продавца + контакты
+
+    # Актуальность объявления
+
+    # Количество товара
+
+    # Возможен ли обмен
+
+    # Адрес продажи/осмотре
+
+    # Б\У товар или нет
+
+    # Возможность взять в долг/в рассрочку
